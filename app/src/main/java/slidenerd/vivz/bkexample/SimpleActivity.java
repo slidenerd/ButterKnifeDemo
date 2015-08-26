89de190f20d2d46fd3d42b4ba8a981b8f8253608
slidenerd <slidenerd@gmail.com>
2015-08-26 16:42:29 +0530

    Initial commit

---
+++ app/src/main/java/slidenerd/vivz/bkexample/SimpleActivity.java
@@ -0,0 +1,84 @@
+package slidenerd.vivz.bkexample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.OnItemClick;
+import butterknife.OnLongClick;
+
+import static android.widget.Toast.LENGTH_SHORT;
+
+public class SimpleActivity extends Activity {
+
+    private static final ButterKnife.Action<View> ALPHA_FADE = new ButterKnife.Action<View>() {
+        @Override
+        public void apply(View view, int index) {
+            AlphaAnimation alphaAnimation = new AlphaAnimation(0, 1);
+            alphaAnimation.setFillBefore(true);
+            alphaAnimation.setDuration(500);
+            alphaAnimation.setStartOffset(index * 100);
+            view.startAnimation(alphaAnimation);
+        }
+    };
+    @Bind(R.id.title)
+    TextView title;
+    @Bind(R.id.subtitle)
+    TextView subtitle;
+    @Bind(R.id.hello)
+    Button hello;
+    @Bind(R.id.list_of_things)
+    ListView listOfThings;
+    @Bind(R.id.footer)
+    TextView footer;
+
+    @Bind({R.id.title, R.id.subtitle, R.id.hello})
+    List<View> headerViews;
+
+    private SimpleAdapter adapter;
+
+
+    @OnClick(R.id.hello) public void sayHello() {
+        Toast.makeText(this, "Disable your adblocker sweethearts :)!", LENGTH_SHORT).show();
+        ButterKnife.apply(headerViews,ALPHA_FADE);
+    }
+
+    @OnLongClick(R.id.hello) boolean sayGetOffMe() {
+        Toast.makeText(this, "Let go of me!", LENGTH_SHORT).show();
+        return true;
+    }
+
+    @OnItemClick(R.id.list_of_things) void handleOnItemClick(int position) {
+        Toast.makeText(this, "You clicked: " + adapter.getItem(position), LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.simple_activity);
+        ButterKnife.bind(this);
+
+        // Contrived code to use the bound fields.
+        title.setText("Butter Knife");
+        subtitle.setText("Field and method binding for Android views.");
+        footer.setText("Originally by Jake Wharton modified by Vivz");
+        hello.setText("I am a clickbait");
+
+        adapter = new SimpleAdapter(this);
+        listOfThings.setAdapter(adapter);
+    }
+
+
+}
---
+++ app/src/main/java/slidenerd/vivz/bkexample/SimpleApp.java
@@ -0,0 +1,10 @@
+package slidenerd.vivz.bkexample;
+
+import android.app.Application;
+
+public class SimpleApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
